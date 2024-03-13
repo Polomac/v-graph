9572646f4e5f25eb8d14ad29520932c4a21d5489
Polomac <milan.polomac@gmail.com>
2024-03-13 14:06:55 +0100

    Minor

--- components.d.ts
+++ components.d.ts
@@ -8,9 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Button: typeof import('./src/components/Button/Button.vue')['default']
-    ElAffix: typeof import('element-plus/es')['ElAffix']
     ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElLink: typeof import('element-plus/es')['ElLink']
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     Loader: typeof import('./src/components/Loader.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
